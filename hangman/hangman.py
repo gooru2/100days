492ac7f6a674a283c2d19f2e0949f0934913b66b
gooru <gooru2@wp.pl>
2024-09-22 20:54:14 +0000

    hangman ended

---
+++ hangman/hangman.py
@@ -0,0 +1,45 @@
+#TODO: Clear console 
+#TODO: repair stages
+
+import random
+from hangman_art import logo, stages
+from hangman_words import word_list
+
+# adding game condition
+chosen_word = random.choice(word_list)
+lives = 6
+
+print(logo)
+
+#crating blank password
+display= []
+for letter in chosen_word:
+    display.append('_')
+
+# while there are any blanks in password
+while '_' in display:
+    #logic for guessing and assign to password
+    guess = input("Guess a letter: ").lower()
+    if guess in display:
+        print(f"You already guess this letter: {guess}")
+
+#adding letter to corect place and print on the screeen
+    for index, letter in enumerate(chosen_word):
+        if letter == guess:
+            display[index] = guess
+
+#guees go bad
+    if not guess in chosen_word:
+        print("This letter is not in the password")
+        lives -= 1
+        #print(stages,lives)
+        print(f"You have {lives} livesc")
+    if lives == 0:
+        print(f"You LOOSER. Correct password is: {chosen_word} ")
+        break
+
+#print gueesed letters
+    print(f"{' '.join(display)}")
+
+if lives!=0:
+    print ("WINNER")
